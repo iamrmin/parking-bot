944d2961e792e7394ffe55ee72c0752dc7acb033
Rashmin Javiya <rashmin.javiya@siemens.com>
2019-10-17 23:00:08 +0530

    feat(App): Initial commit

---
+++ ParkingBot/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingBot
+{
+    public interface IParkingCalculator
+    {
+        float TotalCollection { get; set; }
+        bool IsCoinAllowed(float value);
+        DateTime CalculateTime(float money);
+        bool IsParkingFree();
+    }
+
+    public class EuropeanParking : IParkingCalculator
+    {
+        readonly float[] _allowedCoins = { 0.10f, 0.20f, 0.50f, 1, 2 };
+        private readonly DateTime[] _publicHolidays = { new DateTime(DateTime.Now.Year, 5, 14), new DateTime(DateTime.Now.Year, 12, 25) };
+
+        public float TotalCollection { get; set; }
+
+        /// <summary>
+        /// Method to verify allowed coins
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool IsCoinAllowed(float value)
+        {
+            return _allowedCoins.Contains(value);
+        }
+
+        /// <summary>
+        /// Clear the screen and print the value on screen.
+        /// </summary>
+        /// <param name="money"></param>
+        /// <returns></returns>
+        public DateTime CalculateTime(float money)
+        {
+            TotalCollection += money;
+
+            var result = DateTime.Now;
+
+            if ((int)money / 8 > 0)
+            {
+                result = result.AddDays((int)(money / 8));
+                money = money - (8 * ((int)(money / 8)));
+            }
+
+            if ((int)money / 5 > 0)
+            {
+                result = result.AddHours(12);
+                money -= 5;
+            }
+
+            if ((int)money / 3 > 0)
+            {
+                result = result.AddHours(2);
+                money -= 3;
+            }
+
+            if ((int)money / 2 > 0)
+            {
+                result = result.AddHours(1);
+                money -= 2;
+            }
+
+            if ((int)money / 1 > 0)
+            {
+                result = result.AddMinutes(20);
+                money -= 1;
+            }
+
+            return result;
+        }
+
+        public bool IsParkingFree()
+        {
+            return IsParkingFree(DateTime.Now);
+        }
+
+        public bool IsParkingFree(DateTime dateTime)
+        {
+            return dateTime.Month == 8
+                   || _publicHolidays.Contains(dateTime.Date)
+                   || !(dateTime.DayOfWeek > 0 && (int)dateTime.DayOfWeek < 7
+                        && (dateTime.Hour >= 8 && dateTime <= new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, 12, 0, 0)
+                            || dateTime.Hour >= 14 && dateTime <= new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, 18, 0, 0)));
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IParkingCalculator parkingCalculator = new EuropeanParking();
+
+            // Check for free parking
+            void CheckForFreeParking()
+            {
+                if (parkingCalculator.IsParkingFree())
+                {
+                    ClearAndPrint(new[] { " Parking is free! Enjoy!" });
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                }
+            }
+            CheckForFreeParking();
+
+            // Get user input
+            ClearAndPrint(new[] { " Enter Coin : " });
+            string rawInput = Console.ReadLine();
+
+            // Run contineously till user choose to quit
+            while (rawInput.ToLower() != "q")
+            {
+                // Check if parking is free
+                CheckForFreeParking();
+
+                var coinValue = float.Parse(rawInput);
+
+                // Get the result
+                var isCoinAllowed = parkingCalculator.IsCoinAllowed(coinValue);
+                DateTime result = parkingCalculator.CalculateTime(isCoinAllowed ? coinValue : 0);
+
+                ClearAndPrint(new[]
+                {
+                    !isCoinAllowed ? $" {coinValue} is not a valid coin! Please try again!\n\r" : "",
+                    $" Total money collected : {parkingCalculator.TotalCollection}",
+                    $" You can park your vehicle till : {result}",
+                    " Enter coin : "
+                });
+
+                rawInput = Console.ReadLine();
+            }
+        }
+
+
+        /// <summary>
+        /// Clear the screen and print the value on screen.
+        /// </summary>
+        /// <param name="value"></param>
+        static void ClearAndPrint(string[] value)
+        {
+            Console.Clear();
+            value.ToList().ForEach(a =>
+            {
+                Console.Write(Environment.NewLine + a);
+            });
+        }
+    }
+}
+
+
+
---
+++ ParkingBot/ProgramTests.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ParkingBot
+{
+    [TestFixture]
+    class ProgramTests
+    {
+        public static readonly object[] DateToCheckFreeParking =
+        {
+            new Object[] {new DateTime(2018, 08, 13, 7, 59, 0), true}, // August month
+            new Object[] {new DateTime(2018, 12, 25, 7, 59, 0), true}, // Public holiday
+            new Object[] {new DateTime(2018, 05, 13, 7, 59, 0), true},
+            new Object[] {new DateTime(2018, 05, 13, 8, 0, 0), true},
+            new Object[] {new DateTime(2018, 05, 13, 8, 10, 0), true },
+            new Object[] {new DateTime(2018, 05, 16, 7, 59, 0), true},
+            new Object[] {new DateTime(2018, 05, 16, 8, 0, 0), false},
+            new Object[] {new DateTime(2018, 05, 16, 8, 10, 0), false },
+            new Object[] {new DateTime(2018, 05, 16, 11, 59, 0), false},
+            new Object[] {new DateTime(2018, 05, 16, 12, 0, 0), false },
+            new Object[] {new DateTime(2018, 05, 16, 12, 1, 0), true },
+
+            new Object[] {new DateTime(2018, 05, 13, 14, 0, 0), true},
+            new Object[] {new DateTime(2018, 05, 13, 14, 10, 0), true },
+            new Object[] {new DateTime(2018, 05, 16, 13, 59, 0), true},
+            new Object[] {new DateTime(2018, 05, 16, 14, 0, 0), false},
+            new Object[] {new DateTime(2018, 05, 16, 14, 10, 0), false },
+            new Object[] {new DateTime(2018, 05, 16, 17, 59, 0), false},
+            new Object[] {new DateTime(2018, 05, 16, 18, 0, 0), false },
+            new Object[] {new DateTime(2018, 05, 16, 18, 1, 0), true }
+        };
+
+        public static readonly object[] DateToCheckCalculatedTime =
+        {
+            new Object[] {8, DateTime.Now.AddDays(1)},
+            new Object[] {0, DateTime.Now},
+            new Object[] {0.10f, DateTime.Now},
+            new Object[] {0.20f, DateTime.Now},
+            new Object[] {0.50f, DateTime.Now},
+            new Object[] {1, DateTime.Now.AddMinutes(20)},
+            new Object[] {2, DateTime.Now.AddHours(1)},
+            new Object[] {3, DateTime.Now.AddHours(2)},
+            new Object[] {5, DateTime.Now.AddHours(12)},
+            new Object[] {1.80f, DateTime.Now.AddMinutes(20)},
+            new Object[] {15, DateTime.Now.AddHours(37)},
+            new Object[] {11.70f, DateTime.Now.AddHours(26)}
+        };
+
+        [Test, TestCaseSource(nameof(DateToCheckFreeParking))]
+        public void IsParkingFree_VerifyScenario(DateTime dateTime, bool expectedOutput)
+        {
+            // Setup
+            EuropeanParking europeanParking = new EuropeanParking();
+
+            // Act
+            var actualResult = europeanParking.IsParkingFree(dateTime);
+
+            // Assert
+            Assert.AreEqual(expectedOutput, actualResult);
+        }
+
+        [Test]
+        [TestCase(null, false)]
+        [TestCase(0, false)]
+        [TestCase(0.10f, true)]
+        [TestCase(0.20f, true)]
+        [TestCase(0.50f, true)]
+        [TestCase(0.60f, false)]
+        [TestCase(1, true)]
+        [TestCase(2, true)]
+        [TestCase(16, false)]
+        public void IsCoinAllowed_VerifyScenario(float coinValue, bool expectedOutput)
+        {
+            // Setup
+            EuropeanParking europeanParking = new EuropeanParking();
+
+            // Act
+            var actualResult = europeanParking.IsCoinAllowed(coinValue);
+
+            // Assert
+            Assert.AreEqual(expectedOutput, actualResult);
+        }
+
+        [Test, TestCaseSource(nameof(DateToCheckCalculatedTime))]
+        public void CalculateTime_VerifyScenario(float money, DateTime expectedOutput)
+        {
+            // Setup
+            EuropeanParking europeanParking = new EuropeanParking();
+
+            // Act
+            var actualResult = europeanParking.CalculateTime(money);
+
+            // Assert
+            Assert.AreEqual(expectedOutput.Date, actualResult.Date);
+            Assert.AreEqual(expectedOutput.Hour, actualResult.Hour);
+            Assert.AreEqual(expectedOutput.Minute, actualResult.Minute);
+        }
+    }
+}
